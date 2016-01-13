b489271d8dd73be1fd565216344c3d758ded536e
jsstrn <jessternrays@gmail.com>
2016-01-14 00:17:05 +0800

    Add min and max year selection

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const minYear = 2013
+const maxYear = 2014
 const Chart = require('chart.js')
 const _ = require('lodash')
 
@@ -17,10 +19,64 @@ function getColors () {
   return red + ', ' + green + ', ' + blue
 }
 
-const minYear = 2013
-const maxYear = 2014
-function range (data) {
+function populateSelection (everyYear) {
+  const selectMinYear = document.querySelector('select#min-year')
+  const selectMaxYear = document.querySelector('select#max-year')
+  everyYear.forEach((year) => {
+    const eachMinYear = document.createElement('option')
+    eachMinYear.setAttribute('value', year)
+    eachMinYear.textContent = year
+    selectMinYear.appendChild(eachMinYear)
+    const eachMaxYear = document.createElement('option')
+    eachMaxYear.setAttribute('value', year)
+    eachMaxYear.textContent = year
+    selectMaxYear.appendChild(eachMaxYear)
+  })
+}
+
+function getYearRange (data) {
   return (data.year >= minYear && data.year <= maxYear)
+}
+
+function getEveryYear (rainfall) {
+  const everyYear = []
+  const years = _.groupBy(rainfall, 'year')
+  for (const year in years) {
+    everyYear.push(year)
+  }
+  return everyYear
+}
+
+function addYearToDataset (rainfall) {
+  rainfall.forEach(monthlyRainfall => {
+    const date = monthlyRainfall.month.split('-')
+    monthlyRainfall.year = Number(date[0])
+    monthlyRainfall.month = Number(date[1])
+  })
+  return _.groupBy(rainfall.filter(getYearRange), 'year')
+}
+
+function getDatasets (annualRainfall) {
+  const datasets = []
+  for (const byYear in annualRainfall) {
+    const label = byYear
+    const data = []
+    for (const byMonth in annualRainfall[byYear]) {
+      data.push(annualRainfall[byYear][byMonth].total_rainfall)
+    }
+    const dataset = {
+      label: label,
+      fillColor: 'rgba(' + getColors() + ' ,0.2)',
+      strokeColor: 'rgba(' + getColors() + ' ,1)',
+      pointColor: 'rgba(' + getColors() + ' ,1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(' + getColors() + ' ,1)',
+      data: data
+    }
+    datasets.push(dataset)
+  }
+  return datasets
 }
 
 fetch('assets/data/rainfall.json')
@@ -28,41 +84,16 @@ fetch('assets/data/rainfall.json')
     return res.json()
   })
   .then(rainfall => {
-    rainfall.forEach(monthlyRainfall => {
-      const date = monthlyRainfall.month.split('-')
-      monthlyRainfall.year = Number(date[0])
-      monthlyRainfall.month = Number(date[1])
-    })
-    const annualRainfall = _.groupBy(rainfall.filter(range), 'year')
-
-    function getDatasets () {
-      const datasets = []
-      for (const byYear in annualRainfall) {
-        const label = byYear
-        const data = []
-        for (const byMonth in annualRainfall[byYear]) {
-          data.push(annualRainfall[byYear][byMonth].total_rainfall)
-        }
-        const dataset = {
-          label: label,
-          fillColor: 'rgba(' + getColors() + ' ,0.2)',
-          strokeColor: 'rgba(' + getColors() + ' ,1)',
-          pointColor: 'rgba(' + getColors() + ' ,1)',
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: 'rgba(' + getColors() + ' ,1)',
-          data: data
-        }
-        datasets.push(dataset)
-      }
-      return datasets
-    } // getDataset()
-    const myData = {
+    const annualRainfall = addYearToDataset(rainfall)
+    const everyYear = getEveryYear(rainfall)
+    populateSelection(everyYear)
+    const chartData = {
       labels: months,
-      datasets: getDatasets()
+      datasets: getDatasets(annualRainfall)
     }
     const ctx = document.querySelector('#chart').getContext('2d')
-    const chart = new Chart(ctx).Bar(myData)
+    const chart = new Chart(ctx)
+    chart.Bar(chartData)
   })
   .catch(err => {
     throw err
